f60ddcca19b8d2074ddee44cf5a4a2e7a6ebadd2
prince <prince.sethi@trantorinc.com>
2019-12-06 15:18:37 +0530

    Added Input field Validation Method

--- app/src/main/java/com/example/priceforecast/MainActivity.java
+++ app/src/main/java/com/example/priceforecast/MainActivity.java
@@ -80,6 +80,41 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
     }
 
+    //Function To check if all editText fields have valid values before posting data to server
+    public boolean inputFieldValidation(String length, String width, String height){
+        if (length.equals("")) {
+            lengthEditText.requestFocus();
+            Toast.makeText(this, "Please fill all fields and press calculate", Toast.LENGTH_SHORT).show();
+        } else if (width.equals("")) {
+            widthEditText.requestFocus();
+            Toast.makeText(this, "Please fill all fields and press calculate", Toast.LENGTH_SHORT).show();
+        } else if (height.equals("")) {
+            heightEditText.requestFocus();
+            Toast.makeText(this, "Please fill all fields and press calculate", Toast.LENGTH_SHORT).show();
+        } else if (length.substring(0,1).equals(".")) {
+            lengthEditText.requestFocus();
+            Toast.makeText(this, "Input Number should not start with .(dot)", Toast.LENGTH_SHORT).show();
+        } else if (width.substring(0,1).equals(".")) {
+            widthEditText.requestFocus();
+            Toast.makeText(this, "Input Number should not start with .(dot)", Toast.LENGTH_SHORT).show();
+        } else if (height.substring(0,1).equals(".")) {
+            heightEditText.requestFocus();
+            Toast.makeText(this, "Input Number should not start with .(dot)", Toast.LENGTH_SHORT).show();
+        } else if (length.substring(length.length()-1).equals(".")) {
+            lengthEditText.requestFocus();
+            Toast.makeText(this, "Input Number should not end with .(dot)", Toast.LENGTH_SHORT).show();
+        } else if (width.substring(width.length()-1).equals(".")) {
+            widthEditText.requestFocus();
+            Toast.makeText(this, "Input Number should not end with .(dot)", Toast.LENGTH_SHORT).show();
+        } else if (height.substring(height.length()-1).equals(".")) {
+            heightEditText.requestFocus();
+            Toast.makeText(this, "Input Number should not end with .(dot)", Toast.LENGTH_SHORT).show();
+        } else{
+            return true;
+        }
+        return false;
+    }
+
     //On clicking Calculate button this method is called
     public void postData(View view){
         //To check the state of button and change it if condition is true
@@ -95,39 +130,12 @@ public class MainActivity extends AppCompatActivity {
             heightEditText.setEnabled(true);
             lengthEditText.requestFocus();
         } else {
-
             String width = widthEditText.getText().toString();
             String length = lengthEditText.getText().toString();
             String height = heightEditText.getText().toString();
-            //To check if all editText fields have valid values before posting data to server
-            if (length.equals("")) {
-                lengthEditText.requestFocus();
-                Toast.makeText(this, "Please fill all fields and press calculate", Toast.LENGTH_SHORT).show();
-            } else if (width.equals("")) {
-                widthEditText.requestFocus();
-                Toast.makeText(this, "Please fill all fields and press calculate", Toast.LENGTH_SHORT).show();
-            } else if (height.equals("")) {
-                heightEditText.requestFocus();
-                Toast.makeText(this, "Please fill all fields and press calculate", Toast.LENGTH_SHORT).show();
-            } else if (length.substring(0,1).equals(".")) {
-                lengthEditText.requestFocus();
-                Toast.makeText(this, "Input Number should not start with .(dot)", Toast.LENGTH_SHORT).show();
-            } else if (width.substring(0,1).equals(".")) {
-                widthEditText.requestFocus();
-                Toast.makeText(this, "Input Number should not start with .(dot)", Toast.LENGTH_SHORT).show();
-            } else if (height.substring(0,1).equals(".")) {
-                heightEditText.requestFocus();
-                Toast.makeText(this, "Input Number should not start with .(dot)", Toast.LENGTH_SHORT).show();
-            } else if (length.substring(length.length()-1).equals(".")) {
-                lengthEditText.requestFocus();
-                Toast.makeText(this, "Input Number should not end with .(dot)", Toast.LENGTH_SHORT).show();
-            } else if (width.substring(width.length()-1).equals(".")) {
-                widthEditText.requestFocus();
-                Toast.makeText(this, "Input Number should not end with .(dot)", Toast.LENGTH_SHORT).show();
-            } else if (height.substring(height.length()-1).equals(".")) {
-                heightEditText.requestFocus();
-                Toast.makeText(this, "Input Number should not end with .(dot)", Toast.LENGTH_SHORT).show();
-            } else {
+            //Method Call to check if all editText fields have valid values before posting data to server
+            boolean inputFieldCheck = inputFieldValidation(length, width, height);
+            if(inputFieldCheck){
                 calculatingTextView.setText("Calculating Price");
                 submitButton.setClickable(false);
                 buttonState="fetching_price";
